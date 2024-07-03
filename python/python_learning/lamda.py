dee93eea3ddbcdecdbafe4f3bcaab16827842993
SrikanthG25 <srikanthsk2508@gmail.com>
2024-07-03 16:55:11 +0530

    web

--- python/python_learning/lamda.py
+++ python/python_learning/lamda.py
@@ -6,27 +6,25 @@
 # a=lambda a,b:a+b
 # print(a(10,1))
 
-def product_except_self(nums):
-    length = len(nums)
-    answer = [0] * length
+# def product_except_self(nums):
+#     length = len(nums)
+#     answer = [0] * length
 
-    left_product = 1
-    for i in range(length):
-        answer[i] = left_product
-        print(answer[i])
-        left_product *= nums[i]
+#     left_product = 1
+#     for i in range(length):
+#         answer[i] = left_product
+#         left_product *= nums[i]
 
-    right_product = 1
-    for i in range(length - 1, -1, -1):
-        answer[i] *= right_product
-        print(answer[i])
-        right_product *= nums[i]
+#     right_product = 1
+#     for i in range(length - 1, -1, -1):
+#         answer[i] *= right_product
+#         right_product *= nums[i]
 
-    return answer
+#     return answer
 
-input_list = [1, 2, 3, 4]
-output_list = product_except_self(input_list)
-# print(ou tput_list)
+# input_list = [1, 2, 3, 4]
+# output_list = product_except_self(input_list)
+# print(output_list)
 
 # class ProductArray:
 #     def __init__(self, nums):
@@ -52,3 +50,21 @@ output_list = product_except_self(input_list)
 # product_array = ProductArray(input_list)
 # output_list = product_array.product_except_self()
 # print(output_list)
+
+
+
+
+i = 1
+while True:
+    if i%3 == 0:
+        break
+    print(i)
+ 
+    i += 1
+
+
+p = "Am having knowledge with real time project , in python , django, restframework,API development,manual and unit testing, gitlab, Github , jira ,  mysql , postman APIs , Dbeaver.To developing an projects like Jira ticketing tool in my internship periods and gain more knowledge in real time work environment , leaning  in ML to develop the projects in basically"
+print(len(p))
+
+v ="Am having knowledge with real time project , in python , Django, restframework,API development,manual and unit testing, gitlab, Github , jira ,  mysql , postman APIs , Dbeaver."
+print(len(v))
--- python/python_learning/map_fil_reduce.py
+++ python/python_learning/map_fil_reduce.py
@@ -1,47 +1,46 @@
-'''
+
 
 # Map function
-l=['1','2','3','4','5']
-l=list(map(float,l))
-print(type(l[2]))
+# l=['1','2','3','4','5']
+# l=list(map(float,l))
+# print(type(l[2]),l)
 
-print(type(l[2]))
-for i in range(0,5):
-    l[i]=int(l[i])
-print(type(l[2]))
+# print(type(l[2]))
+# for i in range(0,5):
+#     l[i]=int(l[i])
+# print(type(l[2]))
 
 
 # typecasting str
-l=['1','2','3','4','5']
-l=list(map(int,l))
-for i in l:
-    print(i*i)
+# l=['1','2','3','4','5']
+# l=list(map(int,l))
+# for i in l:
+#     print(i*i)
 
 
-#list square 
-l1=[25,1,5,7,95,35,25]
-l1=list(map(lambda l1:l1*l1,l1))
-print(l1)
-for i in l1:
-    print(i)
+# #list square 
+# l1=[25,1,5,7,95,35,25]
+# l1=list(map(lambda l1:l1*l1,l1))
+# print(l1)
+# for i in l1:
+#     print(i)
 
 
 
 #filter function
-def greater(n):
-    return n>25
-l1=[25,1,5,71,95,35,25]
-l1=list(filter(greater,l1))
-print(l1)
+# def greater(n):
+#     return n>25
+# l1=[25,1,5,71,95,35,25]
+# l1=list(filter(greater,l1))
+# print(l1)
 
 
-'''
 
-from functools import reduce
-l1=[25,1,5,71,95,35,25,3]
 
-l1=reduce(lambda a,b:a+b,l1)
-print(l1)
+# from functools import reduce
+# l1=[25,1,5,71,95,35,25,3]
+# l1=reduce(lambda a,b:a+b,l1)
+# print(l1)
 
 '''
 n=0
@@ -50,3 +49,12 @@ for i in l1:
 print(n)
 
 '''
+
+# s = 'srikanth'
+# sr = s.swapcase()
+# print(s)
+
+x = [9, 8, 7, 6]
+x[2] = 1
+x[x[2]] = 5
+print(x[1] % 3)
